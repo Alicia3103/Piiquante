19912828de48eacd3663e5100d47b3a64aa6bb29
Alicia3103 <charlot.alicia3103@gmail.com>
2022-05-24 18:32:40 +0200

    modify sauce avec delete de l'ancienne image

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 
 
 exports.getAllSauce = (req, res, next) => {
-    Sauce.find().then(sauces=> res.status(200).json(sauces))
+    Sauce.find()
+    .then(sauces=> res.status(200).json(sauces))
+    .catch(error => res.status(400).json({error}))
   };
 
 exports.createSauce = (req, res, next) => {
@@ -45,6 +47,34 @@ exports.createSauce = (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
   };
-  exports.modifySauce = (req, res, next) => {
+  exports.modifySauce =(req, res, next) => {
+    Sauce.findById(req.params.id)
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        
+        const newBody=  updateBody(req)
 
+        Sauce.findByIdAndUpdate( req.params.id, newBody )
+        .then(res.status(201).json({ message : "Sauce modifiée"}))
+        .then(fs.unlink(`images/${filename}`,(err => {
+          if (err) console.log(err);
+          else {
+            console.log("image supprimée")
+          }
+          })))
+        .catch(error => res.status(400).json({ error }))
+        
+        
+      })
+      .catch(error => res.status(404).json({ error }))
   }
+
+  function updateBody(req){
+    if (req.file ==undefined) return req.body
+    
+    const modifyBody = JSON.parse(req.body.sauce)
+    modifyBody.imageUrl = req.protocol+"://"+req.headers.host +"/images/"+req.file.filename
+  
+    return modifyBody
+  }
+  
